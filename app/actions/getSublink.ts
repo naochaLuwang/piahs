9c17316984d874909e097cdc67ca19c51603287e
Somojit Arambam <somoarambam123@gmail.com>
2023-05-23 15:23:30 +0530

    home updated

--- app/actions/getSublink.ts
+++ app/actions/getSublink.ts
@@ -1,4 +1,4 @@
 export async function getSubLink(slug: string): Promise<SubLinkProps> {
-    const response = await fetch(`${process.env.API_URL}/api/sublinks/${slug}`);
-    return response.json();
+  const response = await fetch(`${process.env.API_URL}/api/sublinks/${slug}`);
+  return response.json();
 }
