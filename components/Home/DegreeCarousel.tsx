e8d9834e3951d27628e7d0f494091b4e12830b30
Somojit Arambam <somoarambam123@gmail.com>
2023-05-30 21:48:36 +0530

    courses page added

--- components/Home/DegreeCarousel.tsx
+++ components/Home/DegreeCarousel.tsx
@@ -77,7 +77,7 @@ const DegreeCarousel: React.FC<DegreeCarouselProps> = ({ degrees }) => {
                 <p>12th Standard</p>
               </div>
 
-              <Link href={degree.slug}>
+              <Link href={`courses/${degree.slug}`}>
                 <div className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 mt-10 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
                   <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-950 group-hover:h-full"></span>
                   <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
