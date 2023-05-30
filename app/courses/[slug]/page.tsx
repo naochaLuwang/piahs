ea6d469d11f309600c685ca6aca35a6421729cf9
Somojit Arambam <somoarambam123@gmail.com>
2023-05-30 22:05:46 +0530

    resolving metadata error fixed

--- app/courses/[slug]/page.tsx
+++ app/courses/[slug]/page.tsx
@@ -12,10 +12,7 @@ type Props = {
   params: { slug: string };
 };
 
-export async function generateMetadata({
-  params,
-  searchParams,
-}: Props): Promise<Metadata> {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
   const slug = params.slug;
 
