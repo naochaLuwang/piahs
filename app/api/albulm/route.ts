553093f045d80dc27fedd322a24ae87d9ce8d2cb
Somojit Arambam <somoarambam123@gmail.com>
2023-05-31 21:04:18 +0530

    mobile navbar added

--- app/api/albulm/route.ts
+++ app/api/albulm/route.ts
@@ -3,19 +3,6 @@ import prisma from "../../../lib/prismadb";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const accessToken = request.headers.get("authorization");
-
-  if (!accessToken || !verifyJwt(accessToken)) {
-    return new Response(
-      JSON.stringify({
-        error: "Unauthorized",
-      }),
-      {
-        status: 401,
-      }
-    );
-  }
-
   const album = await prisma.albulm.findMany({
     include: {
       images: true,
